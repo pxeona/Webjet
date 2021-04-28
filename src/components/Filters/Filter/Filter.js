4b20e6af944cf56e96a62bd04824925e747cc794
Pravin Xeona <pravin.xeona21@gmail.com>
2021-04-28 20:50:51 +1000

    Added a dummy filter list in filter component to populate filters in the filter tab

--- src/components/Filters/Filter/Filter.js
+++ src/components/Filters/Filter/Filter.js
@@ -5,11 +5,21 @@ import TextInput from "../../UI/TextInput/TextInput";
 import CheckBox from "../../UI/CheckBox/CheckBox";
 
 const filter = (props) => {
+  let inputElement = null;
+  switch (props.filter) {
+    case "Hotel Name":
+      inputElement = <TextInput type={props.filter} />;
+      break;
+    case "Quality Rating":
+      inputElement = <CheckBox type={props.filter} />;
+      break;
+    default:
+      return;
+  }
   return (
     <div className={styles.Filter}>
       <h5 className={styles.Heading}>{props.filter}</h5>
-      <TextInput />
-      <CheckBox />
+      {inputElement}
     </div>
   );
 };
