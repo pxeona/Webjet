5b5dda78963d1a021b8ae44e406bf3e9634ba85a
Pravin Xeona <pravin.xeona21@gmail.com>
2021-04-28 19:41:16 +1000

    Added a hamburger menu for mobile screens

---
+++ src/components/Navigation/Toolbar/HamburgerMenu/HamburgerMenu.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import styles from "./HamburgerMenu.module.css";
+
+const hamburgerMenu = (props) => (
+  <div className={styles.DrawerToggle} onClick={props.clicked}>
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+);
+
+export default hamburgerMenu;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Logo from "../../Logo/Logo";
+import HamburgerMenu from "./HamburgerMenu/HamburgerMenu";
 
 import styles from "./Toolbar.module.css";
 
 const toolbar = () => (
   <header className={styles.Toolbar}>
+    <HamburgerMenu />
     <div className={styles.Logo}>
       <Logo />
     </div>
