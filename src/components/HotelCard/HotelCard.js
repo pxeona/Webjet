f9c875ffbbf5f62075b2de9640a635f1361f45fd
Pravin Xeona <pravin.xeona21@gmail.com>
2021-04-28 21:21:36 +1000

    Added a hotel card to display the hotel details and added a grid layout in search pane for filter and hotel card

---
+++ src/components/HotelCard/HotelCard.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./HotelCard.module.css";
+
+const hotelcard = (props) => {
+  return <div className={styles.HotelCard}></div>;
+};
+
+export default hotelcard;
--- src/containers/SearchPane/SearchPane.js
+++ src/containers/SearchPane/SearchPane.js
@@ -1,14 +1,50 @@
 import React, { Component } from "react";
 import styles from "./SearchPane.module.css";
+import HotelCard from "../../components/HotelCard/HotelCard";
 
 import Filters from "../../components/Filters/Filters";
 
 class Search extends Component {
+  state = {
+    hotels: [
+      {
+        hotelName: "Oaks on William",
+        rating: 5,
+        roomType: "Studio Apartment (No housekeeping)",
+        price: 282,
+        imgSrc:
+          "https://hotelimages.webjet.com.au/hotels/7000000/6430000/6428700/6428660/215090a4_z.jpg",
+      },
+      {
+        hotelName: "Crowne plaza Melbourne",
+        rating: 3.5,
+        roomType: "Standard room, Non smoking",
+        price: 449,
+        imgSrc:
+          "https://hotelimages.webjet.com.au/hotels/1000000/850000/845400/845390/0259fb01_z.jpg",
+      },
+      {
+        hotelName: "Novotel Melbourne on Collins",
+        rating: 2,
+        roomType: "Deluxe King Room",
+        price: 401,
+        imgSrc:
+          "https://hotelimages.webjet.com.au/hotels/1000000/430000/426600/426574/9c79094d_z.jpg",
+      },
+    ],
+  };
+
   render() {
     return (
       <div className={styles.Search}>
+        <h2 className={styles.Heading}>
+          {this.state.hotels.length} Hotels Available in Melbourne
+        </h2>
         <div className={styles.MainContent}>
           <Filters />
+          <div class={styles.HotelCards}>
+            <HotelCard />
+          </div>
         </div>
       </div>
     );
