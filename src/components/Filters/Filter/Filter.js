4fc808359ac1e1c3b1c25cd2558f9eb17421d64c
Pravin Xeona <pravin.xeona21@gmail.com>
2021-04-28 20:30:01 +1000

    Added a reuable text component for the hotel name filter

--- src/components/Filters/Filter/Filter.js
+++ src/components/Filters/Filter/Filter.js
@@ -1,10 +1,13 @@
 import React from "react";
 import styles from "./Filter.module.css";
 
+import TextInput from "../../UI/TextInput/TextInput";
+
 const filter = (props) => {
   return (
     <div className={styles.Filter}>
       <h5 className={styles.Heading}>{props.filter}</h5>
+      <TextInput />
     </div>
   );
 };
---
+++ src/components/UI/TextInput/TextInput.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+import styles from "./TextInput.module.css";
+
+class TextInput extends Component {
+  state = {
+    inputName: "",
+  };
+
+  render() {
+    return (
+      <div>
+        <input className={styles.TextInput} placeholder="Enter Hotel Name" />
+        <button className={styles.Button}>Go</button>
+      </div>
+    );
+  }
+}
+
+export default TextInput;
